cf3f3824fe81cf982a33c7fd7cc49704314602d1
Heidi Fryzell <hfryzell@gmail.com>
2022-12-09 06:48:09 -0500

    javaScript rendering menu items to DOM

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 import { menu } from '/data.js'
+const menuItems =document.getElementById('menu-items')
 
-console.log(menu)
+let menuText =''
+function renderMenu() {
+    menu.forEach(menuItem => {
+        menuText += `<h2>${menuItem.item}</h2>`
+    });
+    menuItems.innerHTML = menuText
+}
+// renderMenu()
+renderMenu()

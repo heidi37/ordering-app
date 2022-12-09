8fc64a74c360d8c6e17338f292b65a415c0ed43b
Heidi Fryzell <hfryzell@gmail.com>
2022-12-09 06:21:21 -0500

    initial commit, js,css,html files created/ working

---
+++ data.js
@@ -0,0 +1,19 @@
+export const menu = [
+    {
+        emoji: '🍕',
+        item: 'Pizza',
+        ingredients: ['pepperoni', 'mushroom', 'mozzarella'],
+        price: 14,
+    },
+    {
+        emoji: '🍔',
+        item: 'Hamburger',
+        ingredients: ['beef', 'cheese', 'lettuce'],
+        price: 12,
+    },
+    {
+        emoji: '🍺',
+        item: 'Beer',
+        ingredients: ['grain', 'hops', 'yeast','water'],
+        price: 12,
+}]

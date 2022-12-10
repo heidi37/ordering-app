6805df0277b458bca532fc289b0ad4813687392b
Heidi Fryzell <hfryzell@gmail.com>
2022-12-09 19:24:32 -0500

    homepage and menu styled

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ const menuItems =document.getElementById('menu-items')
 let menuText =''
 function renderMenu() {
     menu.forEach(menuItem => {
-        menuText += `<h2>${menuItem.item}</h2>`
+        menuText += `
+        <div class="item">
+            <div class="col-emoji">
+                <h2>${menuItem.emoji}</h2>
+            </div>
+            <div class="col-item-listing">
+                <h3>${menuItem.item}</h3>
+                <p>${menuItem.ingredients.join(', ')}</p>
+                <p class="price">$${menuItem.price}</p>
+            </div>
+            <div class="col-plus-icon">
+                <p class="plus-icon">+</p>
+            </div>
+        </div>
+        <hr>
+        `
     });
     menuItems.innerHTML = menuText
 }

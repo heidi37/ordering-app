a6ee7d069a366e95fd52015cd99aa6477c6575f6
Heidi Fryzell <hfryzell@gmail.com>
2022-12-11 14:36:49 -0500

    adding, removing, totaling order working

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ import { menu } from '/data.js'
 const menuItems =document.getElementById('menu-items')
 const orderDetails =document.getElementById('order-details')
 const totalPrice = document.getElementById('total-price');
+const ordersArray = []
 
-let menuText =''
 function renderMenu() {
+    let menuText =''
     menu.forEach((menuItem, index) => {
         menuText += `
         <div class="item" id="item-${menuItem.id}">
@@ -25,31 +26,51 @@ function renderMenu() {
     });
     menuItems.innerHTML = menuText
 }
-// renderMenu()
+
 renderMenu()
 
-// const plusIcon = document.getElementById('plus-icon');
 const currentOrder = document.getElementById('current-order');
 let total = 0;
 document.addEventListener('click', function(e){
     if(e.target.dataset.add){
-    currentOrder.style.display = 'block'
-    total += menu[e.target.dataset.add].price
-    orderDetails.innerHTML += `
-    <div class="order-line">
-        <h3>${menu[e.target.dataset.add].item}</h3>
-        <button id="remove-button" class="remove-button" data-remove="${menu[e.target.dataset.add].id}">remove</button>
-        <p class="price">$${menu[e.target.dataset.add].price}</p>
-    </div>
-    `
-    totalPrice.innerText = `$${total}`
+        addToOrder(e.target.dataset.add)
     }
     if(e.target.dataset.remove) {
-        alert('ready to remove?')
+        removeFromOrder(e.target.dataset.remove)
     }
     if (e.target.id === 'order-button') {
         alert('complete order clicked')
     }
 })
+
+function addToOrder(orderId){ 
+    orderDetails.innerHTML = ''
+    total = 0;
+    currentOrder.style.display = 'block'
+    ordersArray.push(menu[orderId])
+    renderCurrentOrder()
+
+}
+
+function removeFromOrder(orderRemoveIndex){
+    orderDetails.innerHTML = ''
+    total = 0
+    ordersArray.splice(orderRemoveIndex, 1)
+    renderCurrentOrder()
+}
+
+function renderCurrentOrder() {
+    ordersArray.forEach((orderItem, index) => {
+        orderDetails.innerHTML += `
+        <div class="order-line">
+            <h3>${orderItem.item}</h3>
+            <button id="remove-button" class="remove-button" data-remove="${index}">remove</button>
+            <p class="price">$${orderItem.price}</p>
+        </div>
+        `
+        total += orderItem.price 
+        })
+    totalPrice.innerText = `$${total}`
+}
 
 
